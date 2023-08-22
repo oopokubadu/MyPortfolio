b1591f8309a00eb48e07ed160c5c12613719e741
Larry Ntori <l.ntori@generisdevelopers.com>
2023-08-22 10:42:51 +0000

    portfolio

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ function Hero() {
       </div>
       <div className="relative py-12 bg-white sm:py-16 lg:py-20 xl:py-24">
         <div className="bg-[#F3F5F6]">
-          <div className=" px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <div className="mx-auto sm:px-6 lg:px-8 max-w-7xl">
             <div className="flex lg:h-[20rem] flex-wrap justify-between">
               <div className="max-w-md  lg:-mt-[3rem] mx-auto text-center  lg:mx-0 lg:text-left">
                 <p className="mt-4 text-7xl font-medium text-[#FF7D34] lg:mt-6">
@@ -39,17 +39,17 @@ function Hero() {
                   Advertising
                 </p>
               </div>
-              <div className=" flex flex-col items-start md:flex-row mx-auto lg:mx-0  gap-10">
+              <div className=" flex flex-col items-start md:flex-row mx-auto lg:mx-0 gap-10">
                 <div className="max-w-xl mx-auto ">
                   <img
-                    className="h-auto max-w-sm rounded-3xl lg:-mt-[3rem]"
+                    className="h-auto md:mt-[5.8rem] max-w-sm rounded-3xl lg:-mt-[3rem]"
                     src="https://placehold.co/300x350"
                     alt=""
                   />
                 </div>
                 <div className="max-w-xl mx-auto">
                   <img
-                    className="h-auto max-w-sm rounded-3xl lg:-mt-[6rem]"
+                    className="h-auto md:mt-10 max-w-sm rounded-3xl lg:-mt-[6rem]"
                     src="https://placehold.co/300x400"
                     alt=""
                   />
@@ -60,8 +60,6 @@ function Hero() {
             <section className="py-12 sm:py-16 lg:py-20">
             <div className="mx-auto max-w-7xl">
                 <div className="xl:flex xl:items-center xl:justify-between">
-                    
-
                     <div className="grid items-center grid-cols-1 mt-10 gap-y-6 xl:mt-0 sm:grid-cols-2 sm:gap-y-8 lg:grid-cols-5 lg:gap-x-8">
                         <img className="object-contain w-auto mx-auto h-9" src="https://cdn.rareblocks.xyz/collection/clarity/images/brands/1/logo-vertex.svg" alt="" />
                         <img className="object-contain w-auto mx-auto h-9" src="https://cdn.rareblocks.xyz/collection/clarity/images/brands/1/logo-squarestone.svg" alt="" />
